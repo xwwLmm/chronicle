5628739978ad5282abb2dd5547e070495e981dcc
bloss <1248212664@qq.com>
2018-08-30 01:12:28 +0800

    feat: 增加 footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from 'react';
+import styles from './index.scss';
+
+export default class Footer extends Component {
+
+    render() {
+        return (
+            <div className={styles.footer}>
+                编年史·Sun-Reaver 制作
+            </div>
+        )
+    }
+}
--- src/layouts/BasicLayout.js
+++ src/layouts/BasicLayout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Layout } from 'antd';
 import InnerHeader from '../components/Header';
+import InnerFooter from '../components/Footer';
 
 const { Content, Header, Footer } = Layout;
 
@@ -14,10 +15,13 @@ export default class BasicLayout extends React.PureComponent {
                         <InnerHeader />
                     </Header>
                     <Content style={{ margin: '24px 24px 0', height: '100%' }}>
-                        content
+                        <div style={{background: '#fff', padding: '200px 0'}}>
+                            由于权限原因
+                            此处动态注入 router
+                        </div>
                     </Content>
                     <Footer>
-                        footer
+                        <InnerFooter />
                     </Footer>
                 </Layout>
             </div>
